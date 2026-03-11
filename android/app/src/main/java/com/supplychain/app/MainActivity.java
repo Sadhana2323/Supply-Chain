baf3af8c88b4f524f115947ddad44be2437ba705
Sadhana2323 <Sadhana2323@users.noreply.github.com>
2026-03-11 23:03:24 +0530

    Add Android WebView wrapper and GitHub Actions workflow

---
+++ android/app/src/main/java/com/supplychain/app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.supplychain.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        String webAppUrl = getString(R.string.webapp_url);
+        webView.loadUrl(webAppUrl);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
